070c946b7d804abfd65539bda7b1e93cffd0a6a6
billyhinman712 <41489170+billyhinman712@users.noreply.github.com>
2018-08-24 10:27:53 -0700

    made birds viewable

--- controllers/birds.js
+++ controllers/birds.js
@@ -7,9 +7,18 @@ var router = express.Router();
 //get the authorization helper function
 var loggedIn = require('../middleware/loggedIn');
 
+//require the database
+var db = require('../models');
+
 //define routes
 router.get('/', loggedIn, function(req, res){
-	res.render('birds/index');
+	db.bird.findAll({
+		where: {userId: req.user.id}
+	}).then(function(birds){
+		res.render('birds/index', {birds: birds});
+	}).catch(function(err){
+		res.send('oops');
+	});
 });
 
 module.exports = router;
